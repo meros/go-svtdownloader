f478cbb1c252e26fb44079c5f890d56ee6b41448
Alexander Schrab <alexander.schrab@gmail.com>
2018-06-25 08:08:22 +0200

    Ability to download multiple shows added

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	series := kingpin.Flag("series", "Name of series").Short('s').Required().String()
+	series := kingpin.Flag("series", "Name of series").Short('s').Required().Strings()
 	outDir := kingpin.Flag("outDir", "Base directory to put files").Short('o').Required().String()
 	pushbulletToken := kingpin.Flag("pushbulletToken", "Pushbullet token for notifications").Short('p').String()
 	pushbulletDevice := kingpin.Flag("pushbulletDevice", "Pushbullet device for notifications").Short('d').String()
@@ -25,22 +25,25 @@ func main() {
 	}
 
 	for {
-		eps, _ := eplister.Get(*series)
-		for _, ep := range eps {
-			err := epdownloader.Get(ep, *outDir)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-
-			if pb != nil {
-				dev, err := pb.Device(*pushbulletDevice)
-				if err == nil {
-					err = dev.PushNote("Episode downloaded", ep.Series+" "+ep.Season+" "+ep.Series+" has been downloaded")
+		for _, serie := range *series {
+			log.Println("Fetching series", serie)
+			eps, _ := eplister.Get(serie)
+			for _, ep := range eps {
+				err := epdownloader.Get(ep, *outDir)
+				if err != nil {
+					log.Println(err)
+					continue
 				}
 
-				if err != nil {
-					log.Println("Failed to push notification to device", *pushbulletDevice, err)
+				if pb != nil {
+					dev, err := pb.Device(*pushbulletDevice)
+					if err == nil {
+						err = dev.PushNote("Episode downloaded", ep.Series+" "+ep.Season+" "+ep.Series+" has been downloaded")
+					}
+
+					if err != nil {
+						log.Println("Failed to push notification to device", *pushbulletDevice, err)
+					}
 				}
 			}
 		}
